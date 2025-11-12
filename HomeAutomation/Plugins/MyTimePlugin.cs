50f589486a01ecff39d9b0543ad099a91dd792ce
Niels Tanis <nielstanis@live.nl>
2025-11-12 10:47:58 +0100

    Initial content

---
+++ HomeAutomation/Plugins/MyTimePlugin.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace HomeAutomation.Plugins;
+
+/// <summary>
+/// Simple plugin that just returns the time.
+/// </summary>
+public class MyTimePlugin
+{
+    [KernelFunction, Description("Get the current time")]
+    public DateTimeOffset Time() => DateTimeOffset.Now;
+}
+
+public class Def(string data)
+{
+    public string Data { get; private set; } = data;
+    public string? AnotherData { get; private set; }
+
+    public void Test(string input)
+    {
+        AnotherData = input;
+    }
+}
